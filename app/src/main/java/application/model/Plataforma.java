c6db76b4c7f9e258698b676b36e9140fc0c27fda
Shatoll <123880096+gustavo-h-k-oliveira@users.noreply.github.com>
2023-11-17 16:41:27 +0000

    Versão incompleta do Spring Jogos.

---
+++ app/src/main/java/application/model/Plataforma.java
@@ -0,0 +1,50 @@
+package application.model;
+
+import java.lang.annotation.Inherited;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "plataformas")
+public class plataforma {
+    @Id
+    @GeneratedValue(strategy = GeneratedType.IDENTITY)
+    private long id;
+    @Column(unique = true, nullable = false)
+    private String nome;
+
+    @OneToMany(mappedBy = "plataformas")
+    private Set<Jogo> jogos = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Jogo> getJogos() {
+        return jogos;
+    }
+
+    public void setJogos(Set<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+}
